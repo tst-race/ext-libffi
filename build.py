3e38cf60af4ce18040b667fc92ee1bc0e4a5c850
Kyle Treubig <kyle.treubig@twosixlabs.com>
2023-03-24 12:24:51 -0400

    Format build.py

--- build.py
+++ build.py
@@ -27,7 +27,13 @@ import race_ext_builder as builder
 
 def get_cli_arguments():
     """Parse command-line arguments to the script"""
-    parser = builder.get_arg_parser("libffi", "3.3", 1, __file__, [builder.TARGET_ANDROID_x86_64, builder.TARGET_ANDROID_arm64_v8a])
+    parser = builder.get_arg_parser(
+        "libffi",
+        "3.3",
+        1,
+        __file__,
+        [builder.TARGET_ANDROID_x86_64, builder.TARGET_ANDROID_arm64_v8a],
+    )
     return builder.normalize_args(parser.parse_args())
 
 
@@ -36,11 +42,14 @@ if __name__ == "__main__":
     builder.make_dirs(args)
     builder.setup_logger(args)
 
-    builder.install_packages(args, [
-        "automake=1:1.16.1*",
-        "libtool=2.4.6*",
-        "texinfo=6.7.0*",
-    ])
+    builder.install_packages(
+        args,
+        [
+            "automake=1:1.16.1*",
+            "libtool=2.4.6*",
+            "texinfo=6.7.0*",
+        ],
+    )
 
     builder.fetch_source(
         args=args,
@@ -52,29 +61,49 @@ if __name__ == "__main__":
     env = builder.create_standard_envvars(args)
 
     logging.root.info("Configuring build")
-    builder.execute(args, [
-        "autoreconf",
-        "--install",
-    ], cwd=source_dir, env=env)
+    builder.execute(
+        args,
+        [
+            "autoreconf",
+            "--install",
+        ],
+        cwd=source_dir,
+        env=env,
+    )
 
     target = "x86_64-linux-android" if "x86" in args.target else "aarch64-linux-android"
-    builder.execute(args, [
-        "./configure",
-        "--prefix=/",
-        f"--host={target}",
-        f"--target={target}",
-    ], cwd=source_dir, env=env)
+    builder.execute(
+        args,
+        [
+            "./configure",
+            "--prefix=/",
+            f"--host={target}",
+            f"--target={target}",
+        ],
+        cwd=source_dir,
+        env=env,
+    )
 
     logging.root.info("Building")
-    builder.execute(args, [
-        "make",
-        "-j",
-        args.num_threads,
-    ], cwd=source_dir, env=env)
-    builder.execute(args, [
-        "make",
-        f"DESTDIR={args.install_dir}",
-        "install",
-    ], cwd=source_dir, env=env)
+    builder.execute(
+        args,
+        [
+            "make",
+            "-j",
+            args.num_threads,
+        ],
+        cwd=source_dir,
+        env=env,
+    )
+    builder.execute(
+        args,
+        [
+            "make",
+            f"DESTDIR={args.install_dir}",
+            "install",
+        ],
+        cwd=source_dir,
+        env=env,
+    )
 
     builder.create_package(args)
